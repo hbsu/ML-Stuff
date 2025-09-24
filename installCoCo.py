48ec1d4b0eddc238eeab864586f6489dad72d139
hbsu <122388917+hbsu@users.noreply.github.com>
2025-09-23 20:57:53 -0400

    Delete installCoCo.py

--- installCoCo.py
+++
@@ -1,9 +0,0 @@
-# pip install fiftyone
-import fiftyone.zoo as foz
-dataset = foz.load_zoo_dataset(
-    "coco-2017",
-    split="train",                      # "train" or "validation"
-    label_types=["detections"],
-    classes=["person"],                 # only these classes
-    # max_samples=500,                  # optional limit
-)
